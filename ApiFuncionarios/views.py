c6b2ec33e429463880e9a572efa8784043b91b74
Rhama Krisner <rhamakrisner@gmail.com>
2021-07-27 20:16:36 -0300

    Create Migrations

--- ApiFuncionarios/views.py
+++ ApiFuncionarios/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import ModelFuncionarios
+from .serializer import FuncionariosSerializer
 
-# Create your views here.
+
