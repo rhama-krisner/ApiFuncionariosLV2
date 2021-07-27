ebead9abe427c2333647ec2e27528969b7912f6d
Rhama Krisner <rhamakrisner@gmail.com>
2021-07-27 19:49:46 -0300

    Instalação
    Instalação e configuração Django, Django RestFramework

--- ApiFuncionarios/apps.py
+++ ApiFuncionarios/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppfuncionariosConfig(AppConfig):
+class ApifuncionariosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppFuncionarios'
+    name = 'ApiFuncionarios'
--- setup/settings.py
+++ setup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+*_#8ihqx7i#-3zu@c_et)0$zw8paf2uu5fj-x^!wgj8sf8h@n'
+SECRET_KEY = 'django-insecure-a9*qfy*1(s$zcxa8grlr**vfsuutp@1pyyvmhv1h#mebh@7x#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'ApiFuncionarios',
 ]
 
 MIDDLEWARE = [
