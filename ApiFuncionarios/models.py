e1c25aeff93afb6ca4646d490805c8df44174f11
Rhama Krisner <rhamakrisner@gmail.com>
2021-08-08 14:59:19 -0300

    Update Models

--- ApiFuncionarios/models.py
+++ ApiFuncionarios/models.py
@@ -3,18 +3,25 @@ from django.db import models
 # Create your models here.
 
 class ModelFuncionarios(models.Model):
+    sexoChoice = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('N', 'Nenhuma das opções')
+    )
+
     nome = models.CharField(max_length=255)
-    rg = models.CharField(max_length=255)
+    rg = models.IntegerField(max_length=8)
     orgaoEmissor = models.CharField(max_length=255)
-    cpf = models.CharField(max_length=255)
+    cpf = models.IntegerField(max_length=11)
     nacionalidade = models.CharField(max_length=255)
     endereco = models.CharField(max_length=255)
-    cep = models.CharField(max_length=255)
+    cep = models.CharField(max_length=9)
     cidade = models.CharField(max_length=255)
     estado = models.CharField(max_length=255)
-    dataNascimento = models.CharField(max_length=255)
-    sexo = models.CharField(max_length=255)
+    dataNascimento = models.DateField()
+    sexo = models.CharField(max_length=1, choices=sexoChoice, blank=False, null=False)
     escolaridade = models.CharField(max_length=255)
+    email = models.EmailField(max_length = 255, default = 'Nulo')
 
     def __str__(self):
         return self.nome
